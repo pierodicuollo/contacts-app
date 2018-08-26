e4c34283337a4f06a62c5bda77ff7a97c68e916c
Piero <pdicuollo@gmail.com>
2018-08-26 12:19:16 +0500

    new contact dialogue

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -45,6 +45,47 @@
     </v-toolbar>
     <v-content>
       <router-view/>
+      <v-layout row justify-center>
+    <v-dialog v-model="dialog" persistent max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">New Contact</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12 sm6 md6>
+                <v-text-field label="First name" required></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md6>
+                <v-text-field
+                  label="Last name"
+                  required
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-text-field label="Email" required></v-text-field>
+              </v-flex>
+              <v-flex xs12>
+                <v-text-field label="Phone" type="number" required></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6>
+                <v-textarea
+                  label="Notes"
+                  chips
+                ></v-textarea>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click.native="dialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" flat @click.native="dialog = false">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-layout>
     </v-content>
   </v-app>
 </template>
@@ -66,7 +107,8 @@ export default {
       right: true,
       rightDrawer: false,
       title: 'Contacts Application',
-      searchText: ''
+      searchText: '',
+      dialog: false
     }
   }
 }
