4f144910dfd98e82a792bdbd33e4c2de512f6c93
Piero <pdicuollo@gmail.com>
2018-08-26 11:39:08 +0500

    base front view

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -29,20 +29,8 @@
       :clipped-left="clipped"
     >
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
+      
       <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
     </v-toolbar>
     <v-content>
       <router-view/>
@@ -63,9 +51,6 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2017</span>
-    </v-footer>
   </v-app>
 </template>
 
@@ -75,17 +60,17 @@ export default {
   name: 'App',
   data () {
     return {
-      clipped: false,
+      clipped: true,
       drawer: true,
-      fixed: false,
+      fixed: true,
       items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
+        icon: 'contacts',
+        title: 'All Contacts'
       }],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Contacts Application'
     }
   }
 }
