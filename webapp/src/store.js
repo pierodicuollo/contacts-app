51cd5565830ac22680a38a8f81f43042eef86652
pierodicuollo <pdicuollo@gmail.com>
2018-08-28 14:12:02 +0500

    vuex store set up

--- webapp/src/store.js
+++ webapp/src/store.js
@@ -5,12 +5,48 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    contacts: [],
+    newContact: ''
   },
   mutations: {
-
+    SET_CONTACTS (state, contacts) {
+      state.contacts = contacts
+    },
+    ADD_CONTACT (state, contactsObject) {
+      state.contacts.push(contactsObject)
+    },
+    CLEAR_NEW_CONTACT (state) {
+      state.newContact = ''
+    }
   },
   actions: {
-
+    loadContacts ({ commit }) {
+      axios
+        .get('/contacts')
+        .then(r => r.data)
+        .then(contacts => {
+          commit('SET_CONTACTS', contacts)
+        })
+    },
+    addContact ({ commit, state }) {
+      if (!state.newContact) {
+        return
+      }
+      const contact = {
+        title: state.newContact,
+        completed: false,
+        id: randomId()
+      }
+      axios.post('/contacts', contact).then(_ => {
+        commit('ADD_CONTACT', contact)
+      })
+    },
+    clearNewContact ({ commit }) {
+      commit('CLEAR_NEW_CONTACT')
+    }
+  },
+  getters: {
+    newContact: state => state.newContact,
+    contacts: state => state.contacts
   }
 })
