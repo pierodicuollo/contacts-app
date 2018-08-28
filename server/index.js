ae8349492f7409a8aa643b8b8d2b277d85145878
pierodicuollo <pdicuollo@gmail.com>
2018-08-28 17:54:38 +0500

    email backend

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+var helper = require('sendgrid').mail
+const async = require('async')
 const express = require('express')
 const bodyParser= require('body-parser')
 const app = express()
@@ -5,6 +7,58 @@ app.use(bodyParser.urlencoded({extended: true}))
 var mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
 var db
+
+function sendEmail(
+    parentCallback,
+    fromEmail,
+    toEmails,
+    subject,
+    textContent,
+    htmlContent
+  ) {
+    const errorEmails = [];
+    const successfulEmails = [];
+     const sg = require('sendgrid')   ('__YOUR_APIKEY_CREATED_ON_SENDGRID__');
+     async.parallel([
+      function(callback) {
+        // Add to emails
+        for (let i = 0; i < toEmails.length; i += 1) {
+          // Add from emails
+          const senderEmail = new helper.Email(fromEmail);
+          // Add to email
+          const toEmail = new helper.Email(toEmails[i]);
+          // HTML Content
+          const content = new helper.Content('text/html', htmlContent);
+          const mail = new helper.Mail(senderEmail, subject, toEmail, content);
+          var request = sg.emptyRequest({
+            method: 'POST',
+            path: '/v3/mail/send',
+            body: mail.toJSON()
+          });
+          sg.API(request, function (error, response) {
+            console.log('SendGrid');
+            if (error) {
+              console.log('Error response received');
+            }
+            console.log(response.statusCode);
+            console.log(response.body);
+            console.log(response.headers);
+          });
+        }
+        // return
+        callback(null, true);
+      }
+    ], function(err, results) {
+      console.log('Done');
+    });
+    parentCallback(null,
+      {
+        successfulEmails: successfulEmails,
+        errorEmails: errorEmails,
+      }
+    );
+}
+
 MongoClient.connect('mongodb://pdicuollo:password-123@ds133642.mlab.com:33642/contacts-app-pi', (err, client) => {
     if (err) return console.log(err)
     db = client.db('contacts-app-pi')
@@ -43,6 +97,27 @@ MongoClient.connect('mongodb://pdicuollo:password-123@ds133642.mlab.com:33642/co
                 if (err) throw err;
                 res.send("updated")
             });
+        })
+
+        app.post('/email', (req, res) => {
+            async.parallel([
+                function (callback) {
+                  sendEmail(
+                    callback,
+                    'YOUR_FROM_EMAIL@example.com',
+                    [req.body.email],
+                    req.body.subject,
+                    req.body.body
+                  );
+                }
+              ], function(err, results) {
+                res.send({
+                  success: true,
+                  message: 'Email sent',
+                  successfulEmails: results[0].successfulEmails,
+                  errorEmails: results[0].errorEmails,
+                });
+              });
         })
     })    
 })
